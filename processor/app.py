eda566b42fe6811f02e616388614a057ff83af85
eden-fenster <eden.fenster@gmail.com>
2024-01-14 14:56:29 +0200

    Made a bit of process with the processor. * Connect to Jira in next commit.

--- processor/app.py
+++ processor/app.py
@@ -1,21 +1,38 @@
+from typing import List
+
 import requests
 from flask import Flask, render_template, request
 import logging
 
 app = Flask(__name__)
 
-
 # pylint: disable=logging-fstring-interpolation
 
 # Opening page to input details.
+# List to store received input.
+input_to_translate: List[dict] = []
+# List to store encrypted/decrypted input.
+translated: List[str] = []
 
 
-# First player
-@app.route('/')
+# Input to translate
+@app.route('/translate', methods=['POST'])
 def input_first():
     """Input the input + encrypt/decrypt"""
-    logging.debug("Loading")
+    # Recieving input.
+    logging.debug("Loading, recieving input ")
+    input_to_translate.append(request.get_json())
+    logging.debug(f"Received {input_to_translate}")
+    # Sending it to method that will translate it.
+    # Adding translated input to list.
+
     return "Hello World"
+
+# Return translated input
+@app.route('/')
+def return_translated():
+    """Return translated input"""
+    return "Hello"
 
 
 if __name__ == "__main__":
